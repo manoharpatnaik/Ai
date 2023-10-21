b37f1d411e6f332f15e52392a08d16967850079a
manoharpatnaik <134580310+manoharpatnaik@users.noreply.github.com>
2023-10-21 20:12:33 +0530

    Update tic.py

--- tic.py
+++ tic.py
@@ -1,11 +1,11 @@
 import random
 
 def drawBoard(board):
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('---+---+---')
     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
     print('---+---+---')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
 
 def inputPlayerLetter():
     letter = ''
