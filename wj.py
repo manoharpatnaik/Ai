3d94e4f38c61edf68ac881fb7035c6c5f4985d44
manoharpatnaik <134580310+manoharpatnaik@users.noreply.github.com>
2023-11-06 22:18:53 +0530

    Update wj.py

--- wj.py
+++ wj.py
@@ -1,47 +1,40 @@
-from collections import deque
-
-def water_jug_problem(jug1_capacity, jug2_capacity, target_capacity):
-    visited = set()
-    queue = deque([(0, 0, [])])
-    while queue:
-        current_state = queue.popleft()
-        a, b, steps = current_state
-        if a == target_capacity or b == target_capacity:
-            return steps + [(a, b)]
-        if current_state[:2] in visited:
-            continue
-        visited.add(current_state[:2])
-        # Fill jug 1
-        if a < jug1_capacity:
-            queue.append((jug1_capacity, b, steps + [(a, b)]))
-        # Fill jug 2
-        if b < jug2_capacity:
-            queue.append((a, jug2_capacity, steps + [(a, b)]))
-        # Empty jug 1
-        if a > 0:
-            queue.append((0, b, steps + [(a, b)]))
-        # Empty jug 2
-        if b > 0:
-            queue.append((a, 0, steps + [(a, b)]))
-        # Pour from jug 1 to jug 2
-        pour_amount = min(a, jug2_capacity - b)
-        if pour_amount > 0:
-            queue.append((a - pour_amount, b + pour_amount, steps + [(a, b)]))
-        # Pour from jug 2 to jug 1
-        pour_amount = min(jug1_capacity - a, b)
-        if pour_amount > 0:
-            queue.append((a + pour_amount, b - pour_amount, steps + [(a, b)]))
-    return None
-def main():
-    jug1_capacity = int(input("Enter the capacity of jug 1: "))
-    jug2_capacity = int(input("Enter the capacity of jug 2: "))
-    target_capacity = int(input("Enter the target capacity: "))
-    result = water_jug_problem(jug1_capacity, jug2_capacity, target_capacity)
-    if result:
-        print("Solution:")
-        for step in result:
-            print(f"jug1: {step[0]} jug2: {step[1]}")
+from collections import defaultdict
+import math;
+jug1=int(input("enter the jug1 value:"))
+jug2=int(input("enter the jug2 value:"))
+aim=int(input("enter the aim:"))
+   
+visited = defaultdict(lambda: False)
+def waterJugSolver(amt1, amt2):
+    if (amt1 == aim and amt2 == 0) or (amt2 == aim and amt1 == 0):
+        print(f"Jug1:{amt1} Jug2: {amt2}")
+        return True
+    if visited[(amt1, amt2)] == False:
+        print(f"Jug1:{amt1} Jug2: {amt2}")
+        visited[(amt1, amt2)] = True
+        return (waterJugSolver(0, amt2) or
+                waterJugSolver(amt1, 0) or
+                waterJugSolver(jug1, amt2) or
+                waterJugSolver(amt1, jug2) or
+                waterJugSolver(amt1 + min(amt2, (jug1-amt1)),
+                amt2 - min(amt2, (jug1-amt1))) or
+                waterJugSolver(amt1 - min(amt1, (jug2-amt2)),
+                amt2 + min(amt1, (jug2-amt2))))
     else:
-        print("No solution found.")
-if _name_ == "_main_":
-    main()
+        return False
+def check():
+    if (jug1<=aim) and (jug2<=aim):
+        print("Not Possible")
+        return True
+   
+    elif (jug1/2==jug2 or jug2/2==jug1) and (jug1!=aim and jug2!=aim):
+        print("Not Possible")
+        return True
+    elif(aim%(math.gcd(jug1,jug2))!=0):
+        print("Not Possible")
+        return True
+result=check();
+if result!=True:
+    print("Steps: ")
+    waterJugSolver(0, 0)
+
